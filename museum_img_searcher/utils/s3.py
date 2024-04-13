bf46fcba388cb4cbf4d28bb79217b1eb98be0441
JKearnsl <boyarshinovn@gmail.com>
2024-04-13 18:41:50 +0300

    added s3

--- museum_img_searcher/utils/s3.py
+++ museum_img_searcher/utils/s3.py
@@ -4,9 +4,8 @@ from botocore.client import BaseClient as S3Client
 
 
 def download_file(client: S3Client, bucket: str, key: str) -> io.BytesIO:
-    obj = client.Object(bucket, key)
-    data = io.BytesIO()
-    obj.download_fileobj(data)
-    return data
-
-
+    response = client.get_object(Bucket=bucket, Key=key)
+    file_stream = io.BytesIO()
+    file_stream.write(response['Body'].read())
+    file_stream.seek(0)
+    return file_stream
