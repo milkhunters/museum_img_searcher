11c7682235dbaefe1a277c16de2943b7f8c589bd
JKearnsl <boyarshinovn@gmail.com>
2024-04-13 13:36:56 +0300

    added s3

--- museum_img_searcher/main.py
+++ museum_img_searcher/main.py
@@ -3,11 +3,14 @@ import json
 import time
 from enum import StrEnum
 
+import boto3
 import pika
+from botocore.client import BaseClient as S3Client
 
 from museum_img_searcher.config import load_config
 from museum_img_searcher.searcher import search
 from museum_img_searcher.adder import add
+from utils.s3 import download_file
 
 
 class Command(StrEnum):
@@ -15,21 +18,25 @@ class Command(StrEnum):
     ADD = "add"
 
 
-def callback(route_key):
+def callback(route_key: str, s3_client: S3Client, bucket: str):
     def wrap(ch, method, properties, body: str):
         start_time = time.time()
 
         data = json.loads(body)
         file_id = data["file_id"]
 
+        file = download_file(s3_client, bucket, f"searcher/{file_id}")
+
         if data["command"] == Command.ADD:
             print(f" [x] Received add task for file_id: {file_id}")
-            add(file_id)
+            exhibit_id = data["exhibit_id"]
+
+            add(file, exhibit_id)
             print(f" [x] Add took {time.time() - start_time} seconds")
         elif data["command"] == Command.SEARCH:
             print(f" [x] Received search task for file_id: {file_id}")
             task_id = data["task_id"]
-            result = search(body)
+            result = search(file)
             print(f" [x] Search took {time.time() - start_time} seconds")
 
             ch.basic_publish(
@@ -52,7 +59,7 @@ def callback(route_key):
 def main():
     config = load_config()
 
-    connection = pika.BlockingConnection(
+    rmq_connection = pika.BlockingConnection(
         pika.ConnectionParameters(
             host=config.CONTROL.RABBITMQ.HOST,
             port=config.CONTROL.RABBITMQ.PORT,
@@ -63,7 +70,7 @@ def main():
             )
         )
     )
-    channel = connection.channel()
+    channel = rmq_connection.channel()
 
     channel.queue_declare(
         queue=config.CONTROL.SEARCHER_TASKS_SENDER_ID,
@@ -79,11 +86,27 @@ def main():
         auto_delete=False
     )
 
+    s3_session = boto3.Session(
+        aws_access_key_id=config.S3.ACCESS_KEY_ID,
+        aws_secret_access_key=config.S3.ACCESS_KEY,
+        region_name=config.S3.REGION,
+    )
+
+    s3_client = s3_session.client(
+        "s3",
+        endpoint_url=config.S3.ENDPOINT_URL,
+        use_ssl=False,
+    )
+
     print(" [*] Waiting for messages. To exit press CTRL+C")
 
     channel.basic_consume(
         queue=config.CONTROL.SEARCHER_TASKS_RECEIVER_ID,
-        on_message_callback=callback(config.CONTROL.SEARCHER_TASKS_SENDER_ID)
+        on_message_callback=callback(
+            config.CONTROL.SEARCHER_TASKS_SENDER_ID,
+            s3_client,
+            config.S3.BUCKET
+        ),
     )
 
     channel.start_consuming()
