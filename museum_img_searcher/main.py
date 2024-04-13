033a432aa2d0ace69061ebfe81831c8ef75aa0dd
JKearnsl <boyarshinovn@gmail.com>
2024-04-13 10:31:26 +0300

    initial

---
+++ museum_img_searcher/main.py
@@ -0,0 +1,69 @@
+import time
+
+import pika
+
+from museum_img_searcher.config import load_config
+from museum_img_searcher.searcher import search
+
+
+def callback(route_key):
+    def wrap(ch, method, properties, body):
+        print(" [x] Received msg")
+        start_time = time.time()
+        result = search(body)
+        print(f" [x] Search took {time.time() - start_time} seconds")
+
+        ch.basic_publish(
+            exchange="",
+            routing_key=route_key,
+            body=str(result)
+        )
+
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    return wrap
+
+
+def main():
+    config = load_config()
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=config.CONTROL.RABBITMQ.HOST,
+            port=config.CONTROL.RABBITMQ.PORT,
+            virtual_host=config.CONTROL.RABBITMQ.VHOST,
+            credentials=pika.PlainCredentials(
+                config.CONTROL.RABBITMQ.USERNAME,
+                config.CONTROL.RABBITMQ.PASSWORD
+            )
+        )
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(
+        queue=config.CONTROL.SEARCHER_TASKS_SENDER_ID,
+        durable=True,
+        exclusive=False,
+        auto_delete=False
+    )
+
+    channel.queue_declare(
+        queue=config.CONTROL.SEARCHER_TASKS_RECEIVER_ID,
+        durable=True,
+        exclusive=False,
+        auto_delete=False
+    )
+
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+
+    channel.basic_consume(
+        queue=config.CONTROL.SEARCHER_TASKS_RECEIVER_ID,
+        on_message_callback=callback(config.CONTROL.SEARCHER_TASKS_SENDER_ID)
+    )
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    main()
